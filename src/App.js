e8f733954b2496f40b920342d9f75d33aa5aae38
TheNeonLeon <leon.listo@hotmail.com>
2021-09-29 23:19:48 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -1,57 +1,60 @@
-import './App.scss';
-import tdd from './img/tdd-app.png';
-import { useSpring, animated } from 'react-spring';
+import "./App.scss";
+import tdd from "./img/tdd-app.png";
+import { useSpring, animated } from "react-spring";
 
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
-    <div>
-      <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <ul class="navbar-nav mr-auto">
-          <li class="px-3">
-            <Link to="/">Home</Link>
+      <div>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+          <ul class="navbar-nav mr-auto">
+            <li class="px-3">
+              <Link to="/">Home</Link>
+            </li>
+            <li class="px-3">
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li class="px-3">
+              <Link to="/contact">Contact me</Link>
+            </li>
+          </ul>
+          <li class="icons px-5">
+            <a
+              href="https://www.linkedin.com/in/leon-listo/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <i class="bi bi-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com/TheNeonLeon"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="bi bi-github"></i>
+            </a>
           </li>
-          <li class="px-3">
-            <Link to="/projects">Projects</Link>
-          </li>
-          <li class="px-3">
-            <Link to="/contact">Contact me</Link>
-          </li>
-        </ul>
-        <li class="icons px-5">
-        <a href="https://www.linkedin.com/in/leon-listo/" target="_blank" rel="noreferrer"> <i class="bi bi-linkedin"></i></a>
-        <a href="https://github.com/TheNeonLeon" target="_blank" rel="noreferrer"><i class="bi bi-github"></i></a>
-        </li>
-      </nav>
+        </nav>
 
-      {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-      <Switch>
-      <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/Projects">
-          <Projects />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-        
-      </Switch>
-    </div>
-
-  </Router>
-);
+        <Switch>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/Projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-
 
 function Home() {
   const props = useSpring({
@@ -59,45 +62,54 @@ function Home() {
     from: { opacity: 0 },
     reset: false,
     delay: 200,
-    config: { duration: 800 }
-  })
-return (
-  <animated.div style={props} className="home-container">
-    <h1>Hello, my name is Leon Listo. Welcome to my portfolio.</h1>
-    <h3>Check out my projects</h3>
-    <div class="container-fluid">
-    <div class="row">
-    <div class="col-md">
-      <img class="img-fluid" src={tdd} alt="project"></img>
-      <h4>Built a responsive web app. You can see different events from a static
-JSON file in a user-friendly way. You can register for an event
-with a form. The data in the form is saved in localStorage</h4>
-<a href="https://tdd-vue-app.herokuapp.com/#/" target="_blank" rel="noreferrer"> 
- <i class="bi bi-display"></i>
- </a>
-    <a href="https://github.com/TheNeonLeon/tdd-app" target="_blank" rel="noreferrer">
-      <i class="bi bi-code-slash"></i>
-      </a>
-    </div>
-    <div class="col-md">
-    <img class="img-fluid" src={tdd} alt="project"></img>
-    </div>
-    <div class="col-md">
-    <img class="img-fluid" src={tdd} alt="project"></img>
-    </div>
-   </div>
-  </div>
-  </animated.div>
-  
-)
+    config: { duration: 800 },
+  });
+  return (
+    <animated.div style={props} className="home-container">
+      <h1>Hello, my name is Leon Listo. Welcome to my portfolio.</h1>
+      <h3>Check out my projects</h3>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md">
+            <img class="img-fluid" src={tdd} alt="project"></img>
+            <h4>
+              Built a responsive web app. You can see different events from a
+              static JSON file in a user-friendly way. You can register for an
+              event with a form. The data in the form is saved in localStorage
+            </h4>
+            <a
+              href="https://tdd-vue-app.herokuapp.com/#/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="bi bi-display"></i>
+            </a>
+            <a
+              href="https://github.com/TheNeonLeon/tdd-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="bi bi-code-slash"></i>
+            </a>
+          </div>
+          <div class="col-md">
+            <img class="img-fluid" src={tdd} alt="project"></img>
+          </div>
+          <div class="col-md">
+            <img class="img-fluid" src={tdd} alt="project"></img>
+          </div>
+        </div>
+      </div>
+    </animated.div>
+  );
 }
 
 function Projects() {
-return <h2>Projects</h2>;
+  return <h2>Projects</h2>;
 }
 
 function Contact() {
-  return <h2>Contact</h2>
+  return <h2>Contact</h2>;
 }
 
 export default App;
